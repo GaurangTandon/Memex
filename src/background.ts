0564bcd98730e60982addfa3aa305856ee48b71b
Vincent den Boer <vincent@shishkabab.net>
2018-12-14 16:33:03 +0100

    Progress on restore UI

--- src/background.ts
+++ src/background.ts
@@ -72,3 +72,5 @@ window['eventLog'] = eventLog
 window['directLinking'] = directLinking
 window['customList'] = customList
 window['notifications'] = notifications
+
+console.log('Background script initialized')
